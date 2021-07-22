66d6b8fb78e8dbaf9813a42fb00e951c784062d9
성현Maria 김 <globalkmaria@gmail.com>
2021-07-23 01:49:08 +0900

    [Add file] request file upload

---
+++ src/api/request.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_APIKEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${process.env.REACT_APP_APIKEY}&with_networks=123`,
+  fetchTopRated: `/movie/top_rated?api_key=${process.env.REACT_APP_APIKEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=10749`,
+  fetchDocumentariesMovies: `/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=99`,
+};
+
+export default requests;
